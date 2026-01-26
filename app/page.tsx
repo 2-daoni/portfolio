1ae2196546357f34c2d4f90ded7d1176118e6192
2-daoni <lyumilove9@gmail.com>
2026-01-26 15:07:06 +0900

    skills 아이콘 추가

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProfileContainer from "@/components/intro/ProfileContainer";
-
-export default function Home() {
-  return (
-    <div className="flex text-black mx-auto min-h-screen items-center bg-gray-100 justify-center font-sans">
-      <ProfileContainer />
-    </div>
-  );
-}
